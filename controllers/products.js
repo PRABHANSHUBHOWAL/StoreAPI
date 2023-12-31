7fd8436ea8241145175c093811859c9bdbaa3225
PRABHANSHUBHOWAL <prabhanshubhowal@gmail.com>
2024-01-01 01:57:36 +0530

    Store API proj completed

--- controllers/products.js
+++ controllers/products.js
@@ -1,17 +1,17 @@
-const Product=require('../models/product')
-const getAllProductsStatic = async (req,res) =>{
+const Product = require('../models/product')
+const getAllProductsStatic = async (req, res) => {
     // Filters : Hardcode
     // const products=await Product.find({});
     // const products=await Product.find({featured:true});
-    const products=await Product.find({
+    const products = await Product.find({
         // company:'caressa',
-        featured:false,
-        name : {$regex : 'sue', $options : 'i'}
+        featured: false,
+        name: { $regex: 'sue', $options: 'i' }
     });
-    res.status(200).json({products,nbhits:products.length});
+    res.status(200).json({ products, nbhits: products.length });
 }
 
-const getAllProducts = async (req,res) =>{
+const getAllProducts = async (req, res) => {
     // Filters : Dynamic --> {{URL}}/products?name=bar+stool
     // Approach 1
     // console.log(req.query);
@@ -19,52 +19,71 @@ const getAllProducts = async (req,res) =>{
     // res.status(200).json({products,nbhits:products.length});
 
     // Approach 2
-    const {featured,company,name,sort,fields}=req.query;
-    console.log(req.query);
-    
-    const queryObject={};
-    if(featured){
-        queryObject.featured=featured==='true'?true:false;
+    const { featured, company, name, sort, fields, numericFilters } = req.query;
+    // console.log(req.query);
+
+    const queryObject = {};
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false;
     }
-    if(company){
-        queryObject.company=company;
+    if (company) {
+        queryObject.company = company;
     }
-    if(name){
-        queryObject.name = {$regex : name, $options : 'i'}
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i' }
     }
     // if(sort){
     //     queryObject.sort=sort;
     // }
-    // console.log(queryObject);
-    let result= Product.find(queryObject);
+    
+    
+
+    // Numeric Filters
+    if (numericFilters) {
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte',
+        }
+        const regEx = /\b(<|<=|=|>|>=)\b/g;
+        const options = ['price', 'rating'];
+        let filters = numericFilters.replace(regEx,(match)=>`-${operatorMap[match]}-`);
+        filters = filters.split(',').forEach((item) => {
+            const [fields, operator, value] = item.split('-');
+            if (options.includes(fields)) {
+              queryObject[fields] = { [operator]: Number(value) };
+            }
+        });
+        
+    }
+    let result = Product.find(queryObject);
+
     
     // Sorting
-    if(sort){
+    if (sort) {
         const sortList = sort.split(',').join(' ');
         result = result.sort(sortList);
     }
-    else{
-        result=result.sort('createdAt');
+    else {
+        result = result.sort('createdAt');
     }
 
     // Fields
-    if(fields){
+    if (fields) {
         const fieldList = fields.split(',').join(' ');
         result = result.select(fieldList);
     }
 
-    const page=Number(req.query.page) || 1;
-    const limit=Number(req.query.limit) || 10;
-    const skip=(page-1)*limit;
-    result=result.skip(skip).limit(limit)
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || result.length;
+    const skip = (page - 1) * limit;
+    result = result.skip(skip).limit(limit);
 
-    // Numeric Filters
-    
-
-
-    // console.log(queryObject);
-    products=await result;
-    res.status(200).json({products,nbhits:products.length});
+    console.log(queryObject);
+    products = await result;
+    res.status(200).json({ products, nbhits: products.length });
 
 }
 
@@ -78,7 +97,7 @@ const getAllProducts = async (req,res) =>{
 // }
 
 
-module.exports= {
+module.exports = {
     getAllProducts,
     getAllProductsStatic,
 };
