5746f4c303502cf808e52527fff39763ca0df9ff
PRABHANSHUBHOWAL <prabhanshubhowal@gmail.com>
2023-12-31 21:12:31 +0530

    Implementing filter functions

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,21 @@
+const Product=require('../models/product')
 const getAllProductsStatic = async (req,res) =>{
-    res.status(200).json({msg : 'getAll testing'});
+    // Filters : Hardcode
+    // const products=await Product.find({});
+    // const products=await Product.find({featured:true});
+    const products=await Product.find({
+        company:'caressa',
+        featured:true  
+    });
+    res.status(200).json({products,nbhits:products.length});
 }
 
 const getAllProducts = async (req,res) =>{
-    res.status(200).json({msg : 'getAll'});
+    // Filters : Dynamic --> {{URL}}/products?name=bar+stool
+
+    console.log(req.query);
+    const products=await Product.find(req.query);
+    res.status(200).json({products,nbhits:products.length});
 }
 
 const searchproducts = async (req,res) =>{
